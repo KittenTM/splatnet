343de86101951c857b74c27cb54f0362b7b0219e
KittenTM <aidenplayzroblox123@gmail.com>
2026-02-06 23:01:30 +0100

    implement auto-login on login page

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // note: this is just the default config
 const CONFIG = {
-    API_BASE_URL: "http://127.0.0.1:5000" 
+    API_BASE_URL: "http://192.168.1.177:5000" 
 };
--- users/auth/splatfestival/splatfestivalyay.js
+++ users/auth/splatfestival/splatfestivalyay.js
@@ -1,17 +1,43 @@
-window.onload = function() {
+$(document).ready(function () {
+    const form = $("#auth-form");
+    const errorContainer = $("#auth-error");
     const urlParams = new URLSearchParams(window.location.search);
+    const apiBase = CONFIG.API_BASE_URL;
 
-    if (urlParams.has('error')) {
-        const errorContainer = document.getElementById("auth-error");
-        if (errorContainer) {
-            errorContainer.textContent = "Invalid SFID or Password";
-            errorContainer.style.display = "block"; // Show the red text
-        }
-        
-        //user convenience.....grrr
-        const userField = document.getElementsByName("username")[0];
-        if (userField && urlParams.has('username')) {
-            userField.value = urlParams.get('username');
+    $("#frontend_origin").val(window.location.origin);
+    
+    form.attr("action", apiBase + "/api/v2/sso/spfn/generate_token");
+
+    async function checkExistingSession() {
+        try {
+            const response = await fetch(apiBase + "/api/v1/users/me", {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if (response.ok) {
+                window.location.href = "/friend_list/";
+            }
+        } catch (err) {
+            console.log("auth check failed");
         }
     }
-}
+
+    if (!urlParams.has('error')) {
+        checkExistingSession();
+    }
+
+    if (urlParams.has('error')) {
+        errorContainer.text("Invalid SFID or Password").show();
+        if (urlParams.has('username')) {
+            $("input[name='username']").val(urlParams.get('username'));
+        }
+    }
+
+    form.submit(function() {
+        const submitBtn = $(":submit", this);
+        submitBtn.prop("disabled", true).val("Signing In...");
+        setTimeout(function() {
+            submitBtn.prop("disabled", false).val("Sign In");
+        }, 10000);
+    });
+});
